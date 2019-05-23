80bd8edbf3e101e6ee8de5d6a1a13733ec0ce321
onefless <34096920+onefless@users.noreply.github.com>
2019-05-23 11:31:02 +0800

    Update heatmap2.py

--- heatmap2.py
+++ heatmap2.py
@@ -21,9 +21,35 @@ class ditui_heatmap(object):
                 self.lst.append(False)
         return self.lst
     
+    def categorical_numerical_cols(self,data,cols = None):
+        lon_lat = set(['g.parallel_submit','g.meridian_submit'])
+        if cols == None:
+            self.numeric_cols = list(set(data._get_numeric_data().columns)-lon_lat)
+            self.categorical_cols = list(set(data.columns)-set(self.numeric_cols)-lon_lat)
+        else:
+            self.numeric_cols = []
+            self.categorical_cols = []
+            for i in cols:
+                if i in data[cols].select_dtypes(exclude=["number","bool_"]).columns: #categorical
+                    self.categorical_cols.append(i)
+                else:
+                    self.numeric_cols.append(i)
     
+    def get_selection(self,data,cols = None):
+        '''
+        Input selected cols as a list. 
+        '''
+        data = data.copy()
+        self.categorical_numerical_cols(data,cols)
+        dic = {}
+        for col in self.categorical_cols:
+            dic[col] = list(set(data[col]))
+            print(col,':',dic[col])
+        for col in self.numeric_cols:
+            print(col,': 最大值 {}, 最小值 {}, 缺失值占比 {}'.format(max(data[col]),min(data[col]),data[col].isnull().mean()))   
+            
     
-    def output_map(self,data,selection,file_path = 'heatmap01.html'):
+    def output_map(self,data,selection,file_path = 'heatmap01.html',marker_info = []):
         file_path,data,selection = file_path,data.copy(),selection.copy()
         self.selected_data = data.copy()
 
@@ -49,28 +75,39 @@ class ditui_heatmap(object):
             
         map_osm = folium.Map(location=[35,110],zoom_start=5) 
         HeatMap(self.location).add_to(map_osm) 
+        for i,point in zip(self.selected_data.index,self.location):
+            text = ''
+            for col in marker_info:
+                text = text + col + ': ' + str(self.selected_data[col][i]) + '; \n '
+            folium.Marker(point,tooltip=(text)).add_to(map_osm)
+        map_osm.save(file_path)
         map_osm.save(file_path) 
         print('一共有{}个用户'.format(len(self.location)))
         print('热点图已保存.')
-#%%
+        #%%
 import numpy as np
 import pandas as pd
 
 data_path = r'地图sample.csv'
 data = pd.read_csv(data_path,encoding='gb18030')
 
+heatmap = ditui_heatmap()
+
+cols = ['分部','自填学历','i.age_val','授信额','i.sex_typ']
+heatmap.get_selection(data,cols = cols)
+
 selection = {
         '分部':['郑州','重庆','广州','深圳'],
         'i.sex_typ': ['男'],
         '自填学历':['大专','高中']
-        
         }
 
-heatmap = ditui_heatmap()
-heatmap.output_map(data,selection)
+marker_info = ['i.age_val','g.address_submit']
+heatmap.output_map(data,selection,marker_info = marker_info)
 
 selected_people = heatmap.selected_data  
         
+
         
         
         
